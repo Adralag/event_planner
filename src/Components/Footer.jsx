e1962cd6825f41cbf3e28625de5a96685146439a
Adralag <adedojadaniel@gmail.com>
2025-03-25 10:43:59 +0100

    1

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+const Footer = () => {
+return (
+    <>
+        <footer>
+            <p>&copy; Demola&apos;s Event Planning. All rights reserved.</p>
+        </footer>
+    </>
+)}
+
+    export default Footer
